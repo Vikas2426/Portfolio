513a93101217eedad19b2844d5e25edba6602993
vikas592 <vikas.choubey@successive.tech>
2021-07-19 13:58:54 +0530

    remove Work in Progress tag

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ var mernProjectList = {
 
   tech: [
 
-    "Work in progress! <span class='highlighted-text'>Clone of Amazon e-commerce</span>  website using ReactJS and Firebase Auth",
+    "<span class='highlighted-text'>Clone of Amazon e-commerce</span>  website using ReactJS and Firebase Auth",
     "Clone of Google's Note taking app 'Google Keep' using ReactJS. With <span class='highlighted-text'>theme that defaults to system's theme(Light/Dark)</span>.",
     "ReactJS app to <span class='highlighted-text'>visualize the runtime of different algorithms based on user input.</span> Algorithms for searching,sorting, optimizes arithmetic.",
     "Full-stack to-do list web app with <span class='highlighted-text'>drag-n-drop feature</span> using React-beautiful-DND,  MongoDB, ExpressJs, ReactJS and Node",
