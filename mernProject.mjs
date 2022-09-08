9031ed9f831b69ab3eba3f9ff8449b172c603fd0
Vikas2426 <vikas.cby@gmail.com>
2022-09-08 21:22:57 +0530

    feat: add AlgoX project

--- mernProject.mjs
+++ mernProject.mjs
@@ -1,5 +1,6 @@
 export default {
   names: [
+    "AlgoX",
     "Amazon Clone",
     "Google Keep Clone",
     "Visualizr",
@@ -7,7 +8,7 @@ export default {
   ],
 
   githubLinks: [
-
+"https://github.com/Vikas2426/AlgoX",
     "https://github.com/Vikas2426/amazon-clone",
     "https://github.com/Vikas2426/GoogleKeepClone-React-App",
     "https://github.com/Vikas2426/Visualizr-React",
@@ -15,7 +16,7 @@ export default {
   ],
 
   demoLinks: [
-
+"https://vikas2426.github.io/AlgoX/",
     "https://vikas2426.github.io/amazon-clone/",
     "https://vikas2426.github.io/GoogleKeepClone-React-App/",
     "https://vikas2426.github.io/Visualizr-React",
@@ -23,7 +24,7 @@ export default {
   ],
 
   images: [
-
+"images/AlgoX.webm",
     "images/amazon-clone-min.jpg",
     "images/keep.webm",
     "images/visualizr.webm",
@@ -31,6 +32,7 @@ export default {
   ],
 
   tech: [
+    "<span class='highlighted-text'>Sorting algorithm visualizer</span> using React and Styled-Components",
     "<span class='highlighted-text'>Clone of Amazon e-commerce</span>  website using ReactJS and Firebase Auth",
     "Clone of Google's Note taking app 'Google Keep' using ReactJS. With <span class='highlighted-text'>theme that defaults to system's theme(Light/Dark)</span>.",
     "ReactJS app to <span class='highlighted-text'>visualize the runtime of different algorithms based on user input.</span> Algorithms for searching, sorting, optimizes arithmetic.",
